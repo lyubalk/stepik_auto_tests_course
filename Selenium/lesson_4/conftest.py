c44d2d0caa9b863c8b3323f2340f6d38144f7e8d
lyubalk <116004195+lyubalk@users.noreply.github.com>
2024-03-11 15:58:38 +0500

    final task, preparing the environment

---
+++ Selenium/lesson_4/conftest.py
@@ -0,0 +1,25 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default='en', help='Choose language')
+
+
+@pytest.fixture(scope="session")
+def language(pytestconfig):
+    return pytestconfig.getoption("language")
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    user_language = request.config.getoption('language')
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+    print("\nstart browser for test..")
+    browser = webdriver.Chrome(options=options)
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
