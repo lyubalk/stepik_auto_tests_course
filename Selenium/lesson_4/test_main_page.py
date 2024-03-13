2dc8b9e84db83e73d4da1368e5a2becc003ca3c5
lyubalk <116004195+lyubalk@users.noreply.github.com>
2024-03-13 15:27:18 +0500

    final task, element search

--- Selenium/lesson_4/test_main_page.py
+++ Selenium/lesson_4/test_main_page.py
@@ -9,3 +9,9 @@ def test_guest_can_go_to_login_page(browser, language):
     page.open()
     page.go_to_login_page()
     assert language == "en"
+
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
